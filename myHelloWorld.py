32569dfbaf34608020f4e472489c7363a3691800
Krishna Yelamilli <krishnamyellamilli@gmail.com>
2020-06-16 17:09:55 -0700

    Adding Python code for the greeting

---
+++ myHelloWorld.py
@@ -0,0 +1,4 @@
+print("MyHelloWorld")
+print("\nI am using the GitHub platform for the sharing of my projects and work. Having used CVS in the past, I am able to adopt quickly.")
+print("\nRegards,")
+print("\nKrishna")
